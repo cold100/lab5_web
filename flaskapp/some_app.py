d11c25df30581c979de3f9b1b32174569b8842ff
cold100 <85161595+cold100@users.noreply.github.com>
2021-06-10 17:30:15 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/") 
 def hello():
   return" <html><head></head> <body> Hello World! </body></html>"
-if__name__=="__main__":
+if __name__=="__main__":
   app.run(host='127.0.0.1',port=5000) 
 
  
